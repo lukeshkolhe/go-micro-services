841ee2a9f78cbecafe0cc4daec5392d94c70240a
Lukesh Kolhe <113980054+lukeshomni@users.noreply.github.com>
2024-05-30 23:36:15 +0530

    added Order Model

---
+++ model/order.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type order struct {
+	OrderId     uint64     `json:"order_id"`
+	CustemerId  uuid.UUID  `json:"customer_id"`
+	LineItems   []LineItem `json:"line_items"`
+	OrderStatus string     `json:"status"`
+	CreatedAt   *time.Time `json:"created_at"`
+	ShippedAt   *time.Time `json:"shipped_at"`
+	CompletedAt *time.Time `json:"completed_at"`
+}
+
+type LineItem struct {
+	ItemId   uuid.UUID `json:"item_id"`
+	Quantity int       `json:"quantity"`
+	Price    int       `json:"price"`
+}
